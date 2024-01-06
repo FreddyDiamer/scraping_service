6aff6fd16b43eff8981ca8a04b06e23a3178af92
Malik <diamerfreddy72@gmail.com>
2024-01-06 21:15:13 +0300

    changed home page and added filters

--- scraping/models.py
+++ scraping/models.py
@@ -1,13 +1,44 @@
 from django.db import models
 
-class City:
-  name = models.CharField(max_length=50)
-  slug = models.CharField(max_length=50, blank=True)
 
-  class Meta:
-    verbose_name_plural = 'Cities'
+class City(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.CharField(max_length=50, blank=True, unique=True)
 
-  def __str__(self) -> str:
-    return self.name
+    class Meta:
+        verbose_name_plural = 'Cities'
 
-  
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        return super().save(*args, **kwargs)
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.CharField(max_length=50, blank=True, unique=True)
+
+    class Meta:
+        verbose_name_plural = 'Languages'
+
+    def __str__(self):
+        return self.name
+
+
+class Vacancy(models.Model):
+    url = models.URLField(unique=True)
+    salary = models.CharField(max_length=50)
+    title = models.CharField(max_length=250)
+    company = models.CharField(max_length=50)
+    description = models.TextField()
+    date = models.DateField(null=True, blank=True)
+    lang = models.ForeignKey('Language', on_delete=models.CASCADE)
+    city = models.ForeignKey('city', on_delete=models.CASCADE)
+    timestamp = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = 'Vacancies'
--- scraping/views.py
+++ scraping/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
-import datetime
+from .models import Vacancy
 
+
+menu = [
+    { 'title': 'Vacancies', 'url': 'home' }
+]
 
 def index(request):
-  context = { 'date': datetime.datetime.now(), 'name': 'Dave' }
-  return render(request, 'scraping/index.html', context)
+    object_list = Vacancy.objects.all()
+
+    if request.GET :
+        city = request.GET.get('city')
+        lang = request.GET.get('lang')
+
+        if city:
+            object_list = object_list.filter(city__name__contains=city)
+
+        if lang:
+            object_list = object_list.filter(lang__name__contains=lang)
+
+    context = {
+        'object_list': object_list,
+        'menu': menu,
+        'current_url': 'home'
+    }
+    
+    return render(request, 'scraping/index.html', context)
